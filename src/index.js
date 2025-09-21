1fb39fe5ff243d40385bfd70b86bf089b0d13d3b
mtster <mate.topchishvili@outlook.com>
2025-09-21 06:56:06 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
