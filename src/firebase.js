f8625b58789c5c06681c87b767929a95a9d20f2b
mtster <mate.topchishvili@outlook.com>
2025-09-21 06:38:31 +0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  serverTimestamp
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-FwUy8WLXiYtT46F0f59gr461cEI_zmo",
+  authDomain: "protocol-chat-b6120.firebaseapp.com",
+  databaseURL: "https://protocol-chat-b6120-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "protocol-chat-b6120",
+  storageBucket: "protocol-chat-b6120.appspot.com",
+  messagingSenderId: "969101904718",
+  appId: "1:969101904718:web:8dcd0bc8690649235cec1f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, serverTimestamp };
