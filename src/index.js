91c49195a99cc69032455a982ef05b3b26751448
mtster <mate.topchishvili@outlook.com>
2025-09-21 06:53:51 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './styles.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
