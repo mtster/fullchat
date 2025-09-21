f45f9337a99a6077f5f6e5d5009262440cc29194
mtster <mate.topchishvili@outlook.com>
2025-09-21 07:12:15 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import "./styles.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
